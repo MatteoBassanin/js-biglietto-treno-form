c0d6a794e8e18c664f64f54a6c7a94c774fd3005
MatteoBassanin <matteo.bassanin@gmail.com>
2023-02-23 16:59:13 +0100

    fixed result of the ticket cost

--- js/main.js
+++ js/main.js
@@ -6,22 +6,48 @@
 
 
 const buttonSend = document.querySelector("#send");
-let kmMostrati;
-let etaMostrata;
-let nameMostrato;
+
+
+
 
 buttonSend.addEventListener("click",
 
     function(){
         const eta = document.getElementById("age");
-        const etaMostrata = eta.value;
+        const etaMostrata = parseInt(eta.value);
         console.log(etaMostrata);
         const kiloMetri = document.getElementById("kilo_Metri");
-        const kmMostrati = kiloMetri.value;
+        const kmMostrati = parseInt(kiloMetri.value);
         console.log(kmMostrati);
         const fullNameSurName = document.getElementById("fullname");
         const nameMostrato = fullNameSurName.value;
         console.log(nameMostrato);
+        // const eta = document.getElementById("age");
+        // eta.value
+
+        let baseTicketCost = kmMostrati * 0.21;
+
+        alert(baseTicketCost);
+
+        let newTicketcost;
+
+
+        if (etaMostrata <= 18){
+            newTicketcost = baseTicketCost - (baseTicketCost * 20 / 100);   
+        }else if (etaMostrata >= 65){
+            newTicketcost = baseTicketCost - (baseTicketCost * 40 / 100);   
+        }
+        else{
+            newTicketcost = baseTicketCost;
+        }
+
+        newTicketcost = newTicketcost.toFixed(2);
+
+        
+
+
+
+
     }
 
 
@@ -30,23 +56,23 @@ buttonSend.addEventListener("click",
 
 
 
-let baseTicketCost = kmMostrati * 0.21;
+// let baseTicketCost = kmMostrati * 0.21;
 
-console.log(baseTicketCost);
+// alert(baseTicketCost);
 
-let newTicketcost;
+// let newTicketcost;
 
 
-if (etaMostrata <= 18){
-    newTicketcost = baseTicketCost - (baseTicketCost * 20 / 100);   
-}else if (etaMostrata >= 65){
-    newTicketcost = baseTicketCost - (baseTicketCost * 40 / 100);   
-}
-else{
-    newTicketcost = baseTicketCost;
-}
+// if (etaMostrata <= 18){
+//     newTicketcost = baseTicketCost - (baseTicketCost * 20 / 100);   
+// }else if (etaMostrata >= 65){
+//     newTicketcost = baseTicketCost - (baseTicketCost * 40 / 100);   
+// }
+// else{
+//     newTicketcost = baseTicketCost;
+// }
 
-newTicketcost = newTicketcost.toFixed(2);
+// newTicketcost = newTicketcost.toFixed(2);
 
 
 // console.log(newTicketcost);
