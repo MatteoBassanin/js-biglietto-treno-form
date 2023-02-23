f5983e567b9d59cc902e365f6c092600eb6b65fa
MatteoBassanin <matteo.bassanin@gmail.com>
2023-02-23 16:29:04 +0100

    trying to have a result

--- js/main.js
+++ js/main.js
@@ -6,29 +6,50 @@
 
 
 const buttonSend = document.querySelector("#send");
-
+let kmMostrati;
+let etaMostrata;
+let nameMostrato;
 
 buttonSend.addEventListener("click",
 
     function(){
         const eta = document.getElementById("age");
         const etaMostrata = eta.value;
-        alert(etaMostrata);
-        const kiloMetri = document.querySelector("#kilo_Metri");
+        console.log(etaMostrata);
+        const kiloMetri = document.getElementById("kilo_Metri");
         const kmMostrati = kiloMetri.value;
         console.log(kmMostrati);
-        const fullNameSurName = document.querySelector("#fullname");
+        const fullNameSurName = document.getElementById("fullname");
         const nameMostrato = fullNameSurName.value;
         console.log(nameMostrato);
     }
-
-
 
 
 );
 
 
 
+
+let baseTicketCost = kmMostrati * 0.21;
+
+console.log(baseTicketCost);
+
+let newTicketcost;
+
+
+if (etaMostrata <= 18){
+    newTicketcost = baseTicketCost - (baseTicketCost * 20 / 100);   
+}else if (etaMostrata >= 65){
+    newTicketcost = baseTicketCost - (baseTicketCost * 40 / 100);   
+}
+else{
+    newTicketcost = baseTicketCost;
+}
+
+newTicketcost = newTicketcost.toFixed(2);
+
+
+// console.log(newTicketcost);
 
 
 
@@ -53,3 +74,5 @@ buttonSend.addEventListener("click",
 // newTicketcost = newTicketcost.toFixed(2);
 
 // document.getElementById("cost_ticket").innerHTML = "Il costo del tuo biglietto è di " + newTicketcost ;
+
+
